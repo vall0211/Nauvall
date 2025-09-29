e3aee47f8f8e1ed26ba1903abfe35649502af092
vall0211 <nnauval337@gmail.com>
2025-09-30 06:25:11 +0700

    Day one belajar Flask

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template
-app=Flask (__name__)
+from flask import Flask,render_template
 
-@app.route('/index')
+app = Flask(__name__)
+
+data = {
+    "nama": "Nauval Risqullah",
+    "umur": 16,
+    "alamat": "Jl. Bunga No. 123, Banda Aceh",
+    "pekerjaan": "Software Engineer",
+    "hobi": "Berolahraga"
+}
+
+@app.route('/')
 def index():
-    return render_template('INDEX.html',title= 'INDEX', judul="Ini Halaman Index")
-
-@app.route('/biodata')
-def biodata():
-    return render_template('BIODATA.html',title= 'BIODATA', judul="Ini Halaman Biodata")
+    return render_template('index.html',title="halaman index", isi="Selamat Datang di Website Flask")
 
 @app.route('/about')
 def about():
-    return render_template('ABOUT.html',title= 'ABOUT', judul="Ini Halaman ABOUT")
+    return render_template('about.html',title="Halaman About", isi="ini adalah halaman about")
 
-@app.route('/halo/<name>')
-def halo(name):
-    return "Haloo <strong> Nauvall </strong> !! Apa Kabar?? "
+@app.route('/biodata')
+def biodata():
+    return render_template('biodata.html',title="Biodata", isi=data)
 
 if __name__ == '__main__':
-    app.run (debug=True)
+    app.run(debug=True)
